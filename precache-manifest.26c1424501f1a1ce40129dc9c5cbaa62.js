1628824f82b9637818ab6a279227b6eb85fcf8b8
Erikken-Hione <68046024+Erikken-Hione@users.noreply.github.com>
2020-10-23 15:33:34 +0600

    Updates

---
+++ precache-manifest.26c1424501f1a1ce40129dc9c5cbaa62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b64f277bbe81bf2c8dc045f08d6e6781",
+    "url": "/react_page/index.html"
+  },
+  {
+    "revision": "ccd885c1a93c38021ca2",
+    "url": "/react_page/static/css/main.34c2a9e7.chunk.css"
+  },
+  {
+    "revision": "43f60af1e0544291e90d",
+    "url": "/react_page/static/js/2.2993177f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react_page/static/js/2.2993177f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ccd885c1a93c38021ca2",
+    "url": "/react_page/static/js/main.16045b48.chunk.js"
+  },
+  {
+    "revision": "efb102d03d899417d9c6",
+    "url": "/react_page/static/js/runtime-main.5bdfdac5.js"
+  }
+]);
